bdb8aab4de8b9aeb1a216ef068e9f4a0db320786
Raihan Kabir <maskrklo@gmail.com>
2026-05-13 15:17:59 +0300

    fix: render into document for TanStack Router shellComponent document-level rendering

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,8 @@
-import { StrictMode } from "react";
-  import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom/client";
   import { RouterProvider } from "@tanstack/react-router";
   import { getRouter } from "./router";
-  import "./styles.css";
 
   const router = getRouter();
 
-  const rootElement = document.getElementById("root")!;
-
-  ReactDOM.createRoot(rootElement).render(
-    <StrictMode>
-      <RouterProvider router={router} />
-    </StrictMode>,
-  );
+  ReactDOM.createRoot(document).render(<RouterProvider router={router} />);
   
